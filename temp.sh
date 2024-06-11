2eac0f07578d48f2a186a54f5af2b68db3cc0aa2
Aditya <raghavbhai4545@gmail.com>
2024-06-11 19:28:51 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,16 +1,23 @@
-# mkdir initrd
-cd initrd
+cd initrd/bin
 
-echo '#!/bin/sh' > init
-echo 'mount -t sysfs sysfs /sys' >> init
-echo 'mount -t proc proc /proc' >> init
-echo 'mount -t devtmpfs udev /dev' >> init
-echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
-echo 'clear' >> init
-echo '/bin/sh' >> init
-echo 'poweroff -f' >> init
+for cmd in $(./busybox --list); do
+ln -s ./bin/busybox ./$cmd
+done
 
-chmod -R 777 .
-find . | cpio -o -H newc > ../initrd.img
 
-cd ..
+# # mkdir initrd
+# cd initrd
+
+# echo '#!/bin/sh' > init
+# echo 'mount -t sysfs sysfs /sys' >> init
+# echo 'mount -t proc proc /proc' >> init
+# echo 'mount -t devtmpfs udev /dev' >> init
+# echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
+# echo 'clear' >> init
+# echo '/bin/sh' >> init
+# echo 'poweroff -f' >> init
+
+# chmod -R 777 .
+# find . | cpio -o -H newc > ../initrd.img
+
+# cd ..
