b2ffa574a4ca65e60555ea20638713abc07d580d
Aditya <raghavbhai4545@gmail.com>
2024-06-11 19:34:25 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,23 +1,23 @@
-cd initrd/bin
+# cd initrd/bin
 
-for cmd in $(./busybox --list); do
-ln -s ./bin/busybox ./$cmd
-done
+# for cmd in $(./busybox --list); do
+# ln -s ./bin/busybox ./$cmd
+# done
 
 
-# # mkdir initrd
-# cd initrd
+# mkdir initrd
+cd initrd
 
-# echo '#!/bin/sh' > init
-# echo 'mount -t sysfs sysfs /sys' >> init
-# echo 'mount -t proc proc /proc' >> init
-# echo 'mount -t devtmpfs udev /dev' >> init
-# echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
-# echo 'clear' >> init
-# echo '/bin/sh' >> init
-# echo 'poweroff -f' >> init
+echo '#!/bin/sh' > init
+echo 'mount -t sysfs sysfs /sys' >> init
+echo 'mount -t proc proc /proc' >> init
+echo 'mount -t devtmpfs udev /dev' >> init
+echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
+echo 'clear' >> init
+echo '/bin/sh' >> init
+echo 'poweroff -f' >> init
 
-# chmod -R 777 .
-# find . | cpio -o -H newc > ../initrd.img
+chmod -R 777 .
+find . | cpio -o -H newc > ../initrd.img
 
-# cd ..
+cd ..
