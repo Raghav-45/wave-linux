424ced3255088a200ebe53053a6e33572bfaae86
Aditya <raghavbhai4545@gmail.com>
2024-06-11 18:52:57 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-qemu-system-x86_64 -kernel bzImage -initrd initrd.img -nographic -append 'console=ttyS0'
+#!/bin/bash
+
+# Boolean variable to control the use of -nographic and -append 'console=ttyS0'
+USE_CONSOLE=false
+
+# Basic qemu command
+CMD="qemu-system-x86_64 -kernel bzImage -initrd initrd.img"
+
+# Check the boolean variable and modify the command accordingly
+if [ "$USE_CONSOLE" = true ]; then
+    CMD="$CMD -nographic -append 'console=ttyS0'"
+fi
+
+# Execute the command
+eval $CMD
