60dff2b42411f81649ccaee14b7c1e981ab8da36
Aditya <raghavbhai4545@gmail.com>
2024-06-11 19:06:04 +0530

    Update gen-sharedlinks.py

--- gen-sharedlinks.py
+++ gen-sharedlinks.py
@@ -1,23 +1,19 @@
 import os, subprocess
 
 def get_coreutils_commands():
-    result = subprocess.run(["./workspace/bin/coreutils", "--list"], capture_output=True, text=True)
+    result = subprocess.run(["./initrd/bin/busybox", "--list"], capture_output=True, text=True)
     commands = result.stdout.strip().replace("[", "").replace("]", "").split()
     return commands
 
 def create_symlinks(commands):
-    binary_dir = "workspace/bin"
+    binary_dir = "initrd/bin"
     for cmd in commands:
         symlink_path = os.path.join(binary_dir, cmd)
-        os.system(f"ln -s ./{binary_dir}/coreutils ./{symlink_path}")
+        os.system(f"ln -s ./{binary_dir}/busybox ./{symlink_path}")
         # os.system(f"rm -rf ./{symlink_path}")
         print(f"Created symlink for {cmd}")
 
 commands = get_coreutils_commands()
 # print(commands)
 
-formatted_commands = ["-p uu_" + command for command in commands]
-result = ' '.join(formatted_commands)
-print(result)
-
-# create_symlinks(commands)
+create_symlinks(commands)
