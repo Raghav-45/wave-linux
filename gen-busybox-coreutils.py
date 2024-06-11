5c3c712b8d6d44e03bf7af64a036d3b923d85a76
Aditya <raghavbhai4545@gmail.com>
2024-06-11 21:41:51 +0530

    rename

--- gen-busybox-coreutils.py
+++ gen-busybox-coreutils.py
@@ -10,10 +10,10 @@ def create_symlinks(commands):
     for cmd in commands:
         symlink_path = os.path.join(binary_dir, cmd)
         os.system(f"rm -rf ./{symlink_path}")
-        # os.system(f"ln -s ./busybox ./{symlink_path}") # relative path for that link (./busybox)
+        os.system(f"ln -s /bin/busybox ./{symlink_path}") # absolute linux from source directory path for that link (/bin/busybox)
         print(f"Created symlink for {cmd}")
 
-    os.system(f"for cmd in $(./busybox --list); do ln -s /bin/busybox ./{binary_dir}/$cmd; done")  # IDK why but this works but run this inside initrd/bin directory manually
+    # os.system(f"for cmd in $(./initrd/bin/busybox --list); do ln -s /bin/busybox ./initrd/bin/$cmd; done")  # IDK why but this works but run this inside initrd/bin directory manually
 
 commands = get_coreutils_commands()
 # print(commands)
