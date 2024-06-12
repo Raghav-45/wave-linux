0ca38ba850496b86b60533fb225c2521f2e8a57a
Aditya <raghavbhai4545@gmail.com>
2024-06-12 12:11:04 +0530

    Update generateImage.sh

--- generateImage.sh
+++ generateImage.sh
@@ -1,13 +1,16 @@
 cd initrd
 
+########## GENERATE INIT FILE ##########
 echo '#!/bin/sh' > init
 echo 'mount -t sysfs sysfs /sys' >> init
 echo 'mount -t proc proc /proc' >> init
 echo 'mount -t devtmpfs udev /dev' >> init
 echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
 echo 'clear' >> init
+echo 'echo -e "Welcome to \\e[34mWave \\e[32mLinux \\e[31mLive\\e[0m (/init)"' >> init
 echo '/bin/sh' >> init
 echo 'poweroff -f' >> init
+########## GENERATE INIT FILE ##########
 
 chmod -R 777 .
 find . | cpio -o -H newc > ../initrd.img
