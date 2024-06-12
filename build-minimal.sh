c30571a0c913fab174ffd513319b5822b26d337d
Aditya <raghavbhai4545@gmail.com>
2024-06-12 12:11:02 +0530

    Update build-minimal.sh

--- build-minimal.sh
+++ build-minimal.sh
@@ -61,6 +61,7 @@ cd initrd
     echo 'mount -t devtmpfs udev /dev' >> init
     echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
     echo 'clear' >> init
+    echo 'echo -e "Welcome to \\e[34mWave \\e[32mLinux \\e[31mLive\\e[0m (/init)"' >> init
     echo '/bin/sh' >> init
     echo 'poweroff -f' >> init
     ########## GENERATE INIT FILE ##########
